025c99d069528a4484eb7c7853c7b41ae590dff5
sankalp singh <singhsankalp997@gmail.com>
2023-04-25 14:48:50 +0530

    first-cmmit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyBQzt30Q_4F37vH-U1Uof7rvIiCMAMt_RQ",
+  authDomain: "mygitapp-23f77.firebaseapp.com",
+  databaseURL: "https://mygitapp-23f77-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mygitapp-23f77",
+  storageBucket: "mygitapp-23f77.appspot.com",
+  messagingSenderId: "682769390602",
+  appId: "1:682769390602:web:56969ed9a1e50763a6d3f3",
+  measurementId: "G-P7CBWDM6ER"
+};
